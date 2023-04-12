e0307637721b468ca6b385b93c869cd22d082889
tomelleyz <eleyinmiolasunkanmi@gmail.com>
2023-04-12 16:18:39 +0100

    style: Set up design guidelines and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: {
+        400: "#8D8D8D",
+        500: "#0A0A0A",
+        600: "#5F5F5F",
+        700: "#484848",
+        900: "#1A1A1A",
+        950: "#000000",
       },
+      grey: {
+        50: "#F7F7F7",
+        100: "#F0F0F0",
+        200: "#E8E8E8",
+        300: "#A39D9E",
+        500: "#EDEDED",
+        600: "#F4F4F4",
+        700: "#565C69",
+      },
+      pink: {
+        400: "#FFFBF7",
+        500: "#EF2C5A",
+      },
+    },
+    fontSize: {
+      xs: ["12px", { lineHeight: "16px", letterSpacing: "-0.01em" }],
+      sm: ["14px", { lineHeight: "20px", letterSpacing: "-0.01em" }],
+      base: ["16px", { lineHeight: "24px", letterSpacing: "-0.02em" }],
+      lg: ["18px", { lineHeight: "24px", letterSpacing: "-0.02em" }],
+      xl: ["20px", { lineHeight: "32px", letterSpacing: "-0.02em" }],
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      "general-sans": ["General-Sans", "sans-serif"],
     },
   },
   plugins: [],
-}
+};
